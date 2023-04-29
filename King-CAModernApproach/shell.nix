40b624106a94057615bb8ac38d44507a1ea5ab42
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2023-04-29 14:59:01 -0500

    King: enable Debuginfod

--- King-CAModernApproach/shell.nix
+++ King-CAModernApproach/shell.nix
@@ -6,7 +6,7 @@ let
 in
 mkShell {
   buildInputs = with pkgs; with llvmPackages_latest; [
-    stdenv gdb
+    stdenv (gdb.override { enableDebuginfod = true; })
   ];
   # buildInputs is for dependencies you'd need "at run time",
   # were you to to use nix-build not nix-shell and build whatever you were working on
