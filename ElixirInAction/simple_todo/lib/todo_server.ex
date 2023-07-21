48a2d824202002d5432bfd179ba427a8c478ffd9
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2023-07-21 16:46:52 -0500

    EIA TodoList: make TodoServer use GenServer

--- ElixirInAction/simple_todo/lib/todo_server.ex
+++ ElixirInAction/simple_todo/lib/todo_server.ex
@@ -1,58 +1,59 @@
 defmodule TodoServer do
+  use GenServer
 
   @spec start(atom() | list()) :: pid
   def start(entries \\ nil) do
-    pid = spawn(fn ->
-      cond do
-	      Kernel.is_list(entries) ->
-          loop(TodoList.new(entries))
-        is_nil(entries) ->
-          loop(TodoList.new())
-      end
-    end)
-    Process.register(pid, :todo_server)
-    pid
-  end
-  def stop do
-    Process.exit(Process.whereis(:todo_server), :kill)
-  end
-
-  defp loop(todo_list) do
-    new_todo_list =
-      receive do
-      message -> process_message(todo_list, message)
+    result = GenServer.start(__MODULE__, entries, name: __MODULE__)
+    case result do
+      {:ok, pid} -> pid
+      x -> "Unexpected: #{x}"
     end
-    loop(new_todo_list)
+  end
+  @impl GenServer
+  @spec init(atom() | list()) :: {:ok, map}
+  def init(entries) do
+    {:ok,
+     cond do
+       is_list(entries) ->
+         TodoList.new(entries)
+       is_nil(entries) ->
+         TodoList.new()
+     end
+    }
+  end
+  @spec stop :: :ok
+  def stop do
+    GenServer.stop(TodoServer, :normal)
   end
 
+  @impl GenServer
+  def handle_call(request, _from, todo_list) do
+    case request do
+      {:entries, date} ->
+        {:reply, TodoList.entries(todo_list, date), todo_list}
+    end
+  end
+  @impl GenServer
+  def handle_cast(request, todo_list) do
+    case request do
+      {:add_entry, new_entry} ->
+        {:noreply, TodoList.add_entry(todo_list, new_entry)}
+      {:update_entry, id, entry} ->
+        {:noreply, TodoList.update_entry(todo_list, id, entry)}
+      {:delete_entry, id} ->
+        {:noreply, TodoList.delete_entry(todo_list, id)}
+    end
+  end
   def add_entry(new_entry) do
-    send(:todo_server, {:add_entry, new_entry})
+    GenServer.cast(TodoServer, {:add_entry, new_entry})
   end
   def update_entry(id, entry) do
-    send(:todo_server, {:update_entry, id, entry})
+    GenServer.cast(TodoServer, {:update_entry, id, entry})
   end
   def delete_entry(id) do
-    send(:todo_server, {:delete_entry, id})
+    GenServer.cast(TodoServer, {:delete_entry, id})
   end
   def entries(date) do
-    send(:todo_server, {:entries, self(), date})
-    receive do
-      {:todo_entries, entries} -> entries
-    after
-      5000 -> {:error, :timeout}
-    end
-  end
-  defp process_message(todo_list, {:delete_entry, id}) do
-    TodoList.delete_entry(todo_list, id)
-  end
-  defp process_message(todo_list, {:add_entry, new_entry}) do
-    TodoList.add_entry(todo_list, new_entry)
-  end
-  defp process_message(todo_list, {:update_entry, id, entry}) do
-    TodoList.update_entry(todo_list, id, entry)
-  end
-  defp process_message(todo_list, {:entries, caller, date}) do
-    send(caller, {:todo_entries, TodoList.entries(todo_list, date)})
-    todo_list
+    GenServer.call(TodoServer, {:entries, date})
   end
 end
