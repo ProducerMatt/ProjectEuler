403dae4552485d2fad73d867ce71b7e219db2516
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2023-07-25 15:24:52 -0500

    EIA Todo: fumb,ing with persistent lists

--- ElixirInAction/simple_todo/lib/todo/cache.ex
+++ ElixirInAction/simple_todo/lib/todo/cache.ex
@@ -1,4 +1,7 @@
 defmodule Todo.Cache do
+  @type cache_key :: String.t
+  @type cache_map :: %{cache_key => pid}
+
   def init(_) do
     _ = Todo.Database.start()
     {:ok, %{}}
@@ -6,15 +9,17 @@ defmodule Todo.Cache do
   def start do
     GenServer.start(__MODULE__, nil)
   end
+  @spec server_process(atom | pid | {atom, any} | {:via, atom, any}, cache_key) :: pid
   def server_process(cache_pid, todo_list_name) do
     GenServer.call(cache_pid, {:server_process, todo_list_name})
   end
+  @spec handle_call({:server_process, cache_key}, any, cache_map) :: {:reply, pid, cache_map}
   def handle_call({:server_process, todo_list_name}, _, todo_servers) do
     case Map.fetch(todo_servers, todo_list_name) do
       {:ok, todo_server} ->
         {:reply, todo_server, todo_servers}
       :error ->
-        {_, new_server} = Todo.Server.start()
+        {:ok, new_server} = Todo.Server.start(todo_list_name)
         {
           :reply,
           new_server,
--- ElixirInAction/simple_todo/lib/todo/server.ex
+++ ElixirInAction/simple_todo/lib/todo/server.ex
@@ -4,65 +4,70 @@ defmodule Todo.Server do
   """
   use GenServer
 
-  @spec start() :: {:ok, pid}
-  @spec start(list()) :: {:ok, pid}
-  def start(entries \\ []) do
-    result = GenServer.start(__MODULE__, entries, name: __MODULE__)
+  @spec start(Todo.Cache.cache_key) :: {:ok, pid}
+  def start(name) do
+    result = GenServer.start(__MODULE__, name)
     case result do
       {:ok, pid} -> {:ok, pid}
       x -> throw("Unexpected: #{x}")
     end
   end
   @impl GenServer
-  @spec init(list()) :: {:ok, map}
-  def init(entries) do
-    {:ok,
-     cond do
-       length(entries) > 0 ->
-         Todo.List.new(entries)
-       true ->
-         Todo.List.new()
-     end
-    }
+  @spec init(Todo.Cache.cache_key) :: {:ok, {Todo.Cache.cache_key, nil}, {:continue, :init}}
+  def init(name) do
+    {:ok, {name, nil}, {:continue, :init}}
   end
-  @spec stop :: :ok
-  def stop do
-    GenServer.stop(__MODULE__, :normal)
+  @impl GenServer
+  @spec handle_continue(:init, {Todo.Cache.cache_key, nil}) :: {:noreply, {Todo.Cache.cache_key, Todo.List.t}}
+  def handle_continue(:init, {name, nil}) do
+    todo_list = Todo.Database.get(name) || Todo.List.new()
+    {:noreply, {name, todo_list}}
+  end
+  def stop(pid) do
+    GenServer.stop(pid, :normal)
   end
 
   @impl GenServer
-  def handle_call(request, _from, todo_list) do
+  def handle_call(request, _from, {name, todo_list}) do
     case request do
       {:entries, date} ->
-        {:reply, Todo.List.entries(todo_list, date), todo_list}
+        {:reply, Todo.List.entries(todo_list, date), {name, todo_list}}
     end
   end
   @impl GenServer
-  def handle_cast(request, todo_list) do
+  def handle_cast(request, {name, todo_list}) do
     case request do
       {:add_entry, new_entry} ->
-        {:noreply, Todo.List.add_entry(todo_list, new_entry)}
+        new_list = Todo.List.add_entry(todo_list, new_entry)
+        Todo.Database.store(name, new_list)
+        {:noreply, {name, new_list}}
       {:add_entries, new_entries} ->
-        {:noreply, Todo.List.add_entries(todo_list, new_entries)}
+        new_list = Todo.List.add_entries(todo_list, new_entries)
+        Todo.Database.store(name, new_list)
+        {:noreply, {name, new_list}}
       {:update_entry, id, entry} ->
-        {:noreply, Todo.List.update_entry(todo_list, id, entry)}
+        new_list = Todo.List.update_entry(todo_list, id, entry)
+        Todo.Database.store(name, new_list)
+        {:noreply, {name, new_list}}
       {:delete_entry, id} ->
-        {:noreply, Todo.List.delete_entry(todo_list, id)}
+        new_list = Todo.List.delete_entry(todo_list, id)
+        Todo.Database.store(name, new_list)
+        {:noreply, {name, new_list}}
     end
   end
-  def add_entry(new_entry) do
-    GenServer.cast(__MODULE__, {:add_entry, new_entry})
+  def add_entry(pid, new_entry) do
+    GenServer.cast(__MODULE__, {:add_entry, pid, new_entry})
   end
-  def add_entries(new_entries) do
-    GenServer.cast(__MODULE__, {:add_entries, new_entries})
+  def add_entries(pid, new_entries) do
+    GenServer.cast(__MODULE__, {:add_entries, pid, new_entries})
   end
-  def update_entry(id, entry) do
-    GenServer.cast(__MODULE__, {:update_entry, id, entry})
+  def update_entry(pid, id, entry) do
+    GenServer.cast(__MODULE__, {:update_entry, pid, id, entry})
   end
-  def delete_entry(id) do
-    GenServer.cast(__MODULE__, {:delete_entry, id})
+  def delete_entry(pid, id) do
+    GenServer.cast(__MODULE__, {:delete_entry, pid, id})
   end
-  def entries(date) do
-    GenServer.call(__MODULE__, {:entries, date})
+  def entries(pid, date) do
+    GenServer.call(__MODULE__, {:entries, pid, date})
   end
 end
