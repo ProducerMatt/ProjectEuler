3c788ee966fa063fb192200c923267deb107fc4f
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2023-07-25 15:37:03 -0500

    EIA Todo: fix server tests

--- ElixirInAction/simple_todo/test/todo_server_test.exs
+++ ElixirInAction/simple_todo/test/todo_server_test.exs
@@ -14,42 +14,46 @@ defmodule TodoServerTest do
   @new_entry_added %{date: ~D[2023-12-21], id: 4, title: "Flossing"}
 
   test "Todo.Server add" do
+    Todo.Cache.start()
     entries = Map.values(@example_list.entries)
-    Todo.Server.start()
-    Todo.Server.add_entries(entries)
-    Todo.Server.add_entry(@new_entry)
-    assert Todo.Server.entries(@new_entry.date) == [@new_entry_added]
-    Todo.Server.stop()
+    {:ok, pid} = Todo.Server.start("test_add")
+    Todo.Server.add_entries(pid, entries)
+    Todo.Server.add_entry(pid, @new_entry)
+    assert Todo.Server.entries(pid, @new_entry.date) == [@new_entry_added]
+    Todo.Server.stop(pid)
   end
   test "Todo.Server.entries" do
+    Todo.Cache.start()
     entries = Map.values(@example_list.entries)
-    Todo.Server.start()
-    Todo.Server.add_entries(entries)
+    {:ok, pid} = Todo.Server.start("test_entries")
+    Todo.Server.add_entries(pid, entries)
     expected = [
       @example_list.entries[1],
       @example_list.entries[3],
     ]
-    assert expected == Todo.Server.entries(~D[2023-12-19])
-    Todo.Server.stop()
+    assert expected == Todo.Server.entries(pid, ~D[2023-12-19])
+    Todo.Server.stop(pid)
   end
   test "Todo.Server.update_entry" do
+    Todo.Cache.start()
     entries = Map.values(@example_list.entries)
-    Todo.Server.start()
-    Todo.Server.add_entries(entries)
+    {:ok, pid} = Todo.Server.start("test_update_entries")
+    Todo.Server.add_entries(pid, entries)
     id = 1
     to_update = %{title: "Flossing"}
-    Todo.Server.update_entry(id, to_update)
-    new_list = Todo.Server.entries(~D[2023-12-19])
+    Todo.Server.update_entry(pid, id, to_update)
+    new_list = Todo.Server.entries(pid, ~D[2023-12-19])
     added_entry = Map.merge(@example_list.entries[id], to_update)
     assert List.first(new_list) == added_entry
-    Todo.Server.stop()
+    Todo.Server.stop(pid)
   end
   test "Todo.Server.delete_entry" do
+    Todo.Cache.start()
     entries = Map.values(@example_list.entries)
-    Todo.Server.start()
-    Todo.Server.add_entries(entries)
-    Todo.Server.delete_entry(3)
-    assert Todo.Server.entries(@example_list.entries[1].date) == [@example_list.entries[1]]
-    Todo.Server.stop()
+    {:ok, pid} = Todo.Server.start("test_delete")
+    Todo.Server.add_entries(pid, entries)
+    Todo.Server.delete_entry(pid, 3)
+    assert Todo.Server.entries(pid, @example_list.entries[1].date) == [@example_list.entries[1]]
+    Todo.Server.stop(pid)
   end
 end
