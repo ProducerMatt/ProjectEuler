7743ede56908f92cddc2d69c57aeb1e9343ff23e
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2023-07-31 20:03:44 -0500

    oops

--- ElixirInAction/simple_todo/test/todo_cache_test.exs
+++ ElixirInAction/simple_todo/test/todo_cache_test.exs
@@ -1,18 +1,18 @@
 defmodule Todo.CacheTest do
   use ExUnit.Case
 
-  test "server_process" do     #1
+  test "server_process" do
     Todo.System.start_link()
-    bob_pid = Todo.Cache.server_process("bob")
-    assert bob_pid != Todo.Cache.server_process("alice")
-    assert bob_pid == Todo.Cache.server_process("bob")     #2
+    bob_pid = Todo.Cache.server_process("test_server_process")
+    assert bob_pid != Todo.Cache.server_process("test_nonexistent")
+    assert bob_pid == Todo.Cache.server_process("test_server_process")
   end
   test "to-do operations" do
     Todo.System.start_link()
-    alice = Todo.Cache.server_process("alice")
+    alice = Todo.Cache.server_process("test_todo_ops")
     Todo.Server.add_entry(alice, %{date: ~D[2023-12-19], title: "Dentist"})
     entries = Todo.Server.entries(alice, ~D[2023-12-19])
-    assert [%{date: ~D[2023-12-19], title: "Dentist"}] = entries    #1
+    assert [%{date: ~D[2023-12-19], title: "Dentist"}] = entries
     Todo.Server.reset_db(alice)
   end
 end
